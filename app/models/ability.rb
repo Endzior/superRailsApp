f161b926616c502581f028ec747ec9940e7e3b43
Kacper Stąpor <endzior93@gmail.com>
2015-03-24 14:33:58 +0000

    Fixed some stupid shit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,11 @@ class Ability
     #
       user ||= User.new # guest user (not logged in)
       if user.admin?
-        can :manage, Article
-        can :manage, Comment
+        can :manage, :all
       else
         can :read, :all
         cannot :update, Article
-        cannot :destroy, Comment
+        cannot :destroy, Article
       end
     #
     # The first argument to `can` is the action you are giving the user
