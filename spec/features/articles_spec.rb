6701155cd0a2d3e657e4e504e22240e28133b59b
Kacper Stąpor <endzior93@gmail.com>
2015-04-27 14:03:44 +0000

    removed cancan for article creation, added test test for adding an article as not an admin/redactor also ran tests

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -14,5 +14,15 @@ feature 'articles' do
       expect(page).to have_content(@second_article.title)
       expect(page).to have_content('© Endzior 2015')
     end
+    
+    it 'user creates a new article' do
+      visit management_article_path
+      
+      click_link('New article')
+      
+      expect(page).to have_content('You are not authorized to access this page.')
+      expect(page).not_to have_content(@article.title)
+      expect(page.current_url).to eq 'http://www.example.com/'
+    end
   end
 end
