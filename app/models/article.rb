59ad712130c1cf51cb5b4a81625863b3118a8dfe
Kacper Stąpor <endzior93@gmail.com>
2015-04-28 12:14:19 +0000

    added a picture to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_attached_file :picture, :styles => { :medium => "300x300", :thumb => "100x100" }
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
