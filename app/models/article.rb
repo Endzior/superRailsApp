f626384e1b6f3effc44385017ee51ffea72d2ae1
Kacper Stąpor <endzior93@gmail.com>
2015-04-28 13:04:55 +0000

    added additional validation for file type and size

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_attached_file :picture, :styles => { :medium => "300x300", :thumb => "100x100" }
+  validates_attachment_file_name :picture, :matches => [/gif\Z/, /png\Z/, /jpe?g\Z/]
+  validates_with AttachmentSizeValidator, :attributes => :picture, :less_than => 1.megabytes
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   validates :title, presence: true,
                     length: { minimum: 5 }
