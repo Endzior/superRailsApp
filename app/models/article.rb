8c58ff18abe623b8f2bcba9aa12b28fbb22d4918
Kacper Stąpor <endzior93@gmail.com>
2015-04-28 12:21:52 +0000

    added migration to db, article now has a pic in db

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_attached_file :picture, :styles => { :medium => "300x300", :thumb => "100x100" }
+  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
